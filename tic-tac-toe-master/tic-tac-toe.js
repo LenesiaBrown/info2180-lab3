25994aa64c72a24d91378b1933f4777ff2d38050
Lenesia Brown <lenesiabrown37@gmail.com>
2022-10-13 19:55:49 -0500

    Excercise 1 completed

---
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -0,0 +1,57 @@
+
+
+
+window.onload = function(){    
+    
+    document.getElementsByTagName("div")[3].setAttribute("id", "1");
+    let box1 = document.getElementById("1");
+    box1.classList.add("square");
+    console.log(box1);   
+   
+    document.getElementsByTagName("div")[4].setAttribute("id", "2");
+    let box2 = document.getElementById("2");
+    box2.classList.add("square");
+    console.log(box2);
+
+    document.getElementsByTagName("div")[5].setAttribute("id","3");
+    let box3 = document.getElementById("3");
+    box3.classList.add("square");
+    console.log(box3);
+
+    document.getElementsByTagName("div")[6].setAttribute("id","4");
+    let box4 = document.getElementById("4");
+    box4.classList.add("square");
+    console.log(box4);
+
+    document.getElementsByTagName("div")[7].setAttribute("id","5");
+    let box5 = document.getElementById("5");
+    box5.classList.add("square");
+    console.log(box5);
+
+    document.getElementsByTagName("div")[8].setAttribute("id","6");
+    let box6 = document.getElementById("6");
+    box6.classList.add("square");
+    console.log(box6);
+
+    document.getElementsByTagName("div")[9].setAttribute("id","7");
+    let box7 = document.getElementById("7");
+    box7.classList.add("square");
+    console.log(box7);
+
+    document.getElementsByTagName("div")[10].setAttribute("id","8");
+    let box8 = document.getElementById("8");
+    box8.classList.add("square");
+    console.log(box8);
+
+    document.getElementsByTagName("div")[11].setAttribute("id","9");
+    let box9 = document.getElementById("9");
+    box9.classList.add("square");
+    console.log(box9);  
+
+   
+
+
+}
+
+
+
