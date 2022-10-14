a32ae4c181a2989ac924980e4698ece9e0dee355
Lenesia Brown <lenesiabrown37@gmail.com>
2022-10-14 14:42:09 -0500

    Excercise 2 completed and also adjustments made to excercise 1

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -1,57 +1,52 @@
+const emptyBox = [null, null, null, null, null, null, null, null, null]
+const playerX = "X";
+const playerO = "O";
+let firstPlay = playerX;
 
-
-
-window.onload = function(){    
+//Excercise 1
+window.onload = function(){   
+    for (var n = 3; n <= 11; n++) {
+        document.getElementsByTagName("div")[n].setAttribute("id", n);
+        let box1 = document.getElementById(n);
+        box1.classList.add("square"); 
+    }
     
-    document.getElementsByTagName("div")[3].setAttribute("id", "1");
-    let box1 = document.getElementById("1");
-    box1.classList.add("square");
-    console.log(box1);   
-   
-    document.getElementsByTagName("div")[4].setAttribute("id", "2");
-    let box2 = document.getElementById("2");
-    box2.classList.add("square");
-    console.log(box2);
 
-    document.getElementsByTagName("div")[5].setAttribute("id","3");
-    let box3 = document.getElementById("3");
-    box3.classList.add("square");
-    console.log(box3);
-
-    document.getElementsByTagName("div")[6].setAttribute("id","4");
-    let box4 = document.getElementById("4");
-    box4.classList.add("square");
-    console.log(box4);
-
-    document.getElementsByTagName("div")[7].setAttribute("id","5");
-    let box5 = document.getElementById("5");
-    box5.classList.add("square");
-    console.log(box5);
-
-    document.getElementsByTagName("div")[8].setAttribute("id","6");
-    let box6 = document.getElementById("6");
-    box6.classList.add("square");
-    console.log(box6);
-
-    document.getElementsByTagName("div")[9].setAttribute("id","7");
-    let box7 = document.getElementById("7");
-    box7.classList.add("square");
-    console.log(box7);
-
-    document.getElementsByTagName("div")[10].setAttribute("id","8");
-    let box8 = document.getElementById("8");
-    box8.classList.add("square");
-    console.log(box8);
-
-    document.getElementsByTagName("div")[11].setAttribute("id","9");
-    let box9 = document.getElementById("9");
-    box9.classList.add("square");
-    console.log(box9);  
-
+    // gameBoxes.forEach(box => {
+    //     box.addEventListener("click", forClick, {once : true})
+    //     console.log(box);
+    
+    // })
+    
    
 
+//Excercise 2
+    const gameBoxes = Array.from(document.querySelectorAll(".square"));
+    console.log(gameBoxes);
 
+    gameBoxes.forEach(box => {
+        box.addEventListener("click", forClick)
+    });
+    // const boards = Array(gameBoxes.length);
+    // boards.fill(null);
+    
 }
+
+
+// Come back to later!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    function forClick(c) {
+        const tile = c.target.id;
+        //console.log(id);
+        if (!emptyBox[tile]) {
+            emptyBox[tile] = firstPlay;
+            c.target.innerText = firstPlay;
+            firstPlay = firstPlay === playerX ? playerO : playerX
+        }
+    };
+
+    
+
+
 
 
 
